0f490256ba13884615b75709dfd5d0b6f5a57ac1
Ben Amor <benvolio.amore@gmail.com>
2017-09-08 10:16:04 +1200

    Cleaned up some specs

--- spec/get_response_to_guess_spec.rb
+++ spec/get_response_to_guess_spec.rb
@@ -4,71 +4,58 @@ require 'game_state'
 require 'guess_validator'
 
 RSpec.describe GetResponseToGuess do
+  let(:guess_validator) { instance_double(GuessValidator, is_valid_input: is_valid_input) }
+  let(:mock_game_state) { instance_double(GameState, characters_guessed: characters_guessed, current_word: 'Lorem') }
+  let(:characters_guessed) { [] }
 
-  let(:guess_validator) { instance_double(GuessValidator, :is_valid_input => is_valid_input) }
-  let(:mock_game_state) { instance_double(GameState, :characters_guessed => characters_guessed, :current_word => 'Lorem') }
+  let(:response_symbol) { get_response_to_guess.call }
 
-  let(:response_message) { get_response_to_guess.call }
+  subject(:get_response_to_guess) { described_class.new(guess_validator, character_input, mock_game_state) }
 
-  subject(:get_response_to_guess) { described_class.new(guess_validator, character_input, game_state) }
-
-  context 'when the character is invalid' do
+  context 'when the input is invalid' do
     let(:is_valid_input) { false }
     let(:character_input) { '' }
-    let(:game_state) { nil }
-    let(:characters_guessed) { [] }
 
     it 'indicates that the guess is invalid' do
-      expect(response_message).to include('not a valid guess.')
+      expect(response_symbol).to eq(:invalid_input)
     end
   end
 
   context 'when the game state already contains the guess' do
     let(:is_valid_input) { true }
     let(:character_input) { 'x' }
-    let(:game_state) { mock_game_state }
     let(:characters_guessed) { ['x'] }
 
     it 'indicates that the guess has already been guessed' do
-      expect(response_message).to include('already guessed')
+      expect(response_symbol).to eq(:already_guessed)
     end
   end
 
   context 'when the input character is present in the word' do
-    let(:is_valid_input){ true }
-    let(:character_input){'L'}
-    let(:game_state) { mock_game_state }
+    let(:is_valid_input) { true }
+    let(:character_input) { 'o' }
 
     it 'indicates that the guess is succesful' do
-      expect(sut.call).to include('appears')
+      expect(response_symbol).to eq(:hit)
     end
   end
 
-  context '' do
-    it 'should return the appropriate message if the input character is not in the word' do
+  context 'when the upper case variant of the input character is present in the word' do
+    let(:is_valid_input) { true }
+    let(:character_input) { 'l' }
 
-      # Arrange
-      guess_validator = instance_double(GuessValidator, :is_valid_input => true)
-      sut = GetResponseToGuess.new(guess_validator, 'a', mock_game_state)
-
-      # Assert
-      expect(sut.call).to include("doesn't appear")
-
+    it 'indicates that the guess is succesful' do
+      expect(response_symbol).to eq(:hit)
     end
   end
 
-  it 'should be case insensitive when checking if a character appears in the word' do
+  context 'when the input character is not in the word' do
+    let(:is_valid_input) { true }
+    let(:characters_guessed) { ['x'] }
+    let(:character_input) { 'y' }
 
-    # Arrange
-    guess_validator = instance_double(GuessValidator, :is_valid_input => true)
-    sut = GetResponseToGuess.new(guess_validator, 'l', mock_game_state)
-
-    # Act
-    result = sut.call
-
-    # Assert
-    expect(result).to include('appears')
-
+    it 'indicates that the character was not present' do
+      expect(response_symbol).to eq(:miss)
+    end
   end
-
 end
--- spec/guess_validator_spec.rb
+++ spec/guess_validator_spec.rb
@@ -7,21 +7,18 @@ describe GuessValidator do
 
   ['a', 'b'].each do |char|
     it 'should return true for alpha characters' do
-
       expect(guess_validator.is_valid_input(char)).to be_truthy
     end
   end
 
   ['A', 'B'].each do |char|
     it 'should be case insensitive' do
-      byebug
       expect(guess_validator.is_valid_input(char)).to be_truthy
     end
   end
 
   ['1', ' ', ','].each do |char|
     it 'should return false for non alpha characters' do
-
       expect(guess_validator.is_valid_input(char)).to be_falsey
     end
   end
